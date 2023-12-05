b85c4aaea604ec66c1b1f9d648543f3d8b29de94
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-12-05 23:15:03 +0530

    Update MongoDB URI in db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
-const mongoURI = `${process.env.MONGO_URI}`;
+const mongoURI = `${process.env.MONGO_URI}/noteGemini-Database`;
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI).then(() => {
